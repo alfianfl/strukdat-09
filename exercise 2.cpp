30aa13ac5870a9796ca4efec58c1b090c279d27f
alfianfl <alvian.wadada@gmail.com>
2019-05-07 10:52:43 +0700

    strukdat-09

--- exercise 2.cpp
+++ exercise 2.cpp
@@ -1,3 +1,4 @@
+
 /*
 Nama Program  : Exercise-02, Strukdat-09
 Nama    : Alfian Fadhil
@@ -6,70 +7,76 @@ Tanggal Pembuatan : Selasa, 07 Mei 2019
 Deskripsi   : membalikan kata menggunakan linked list stack
 */
  
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-const int maxElemen = 10; 
-struct Stack { 
-    char isi[maxElemen]; 
-    int TOP; 
-}; 
-Stack S; 
+struct List{
+	char isi;
+	List* next;
+};
+typedef List* pointer;
+typedef pointer Stack;
+Stack S;
 
-void createStack (Stack& S){ 
-    S.TOP = -1; 
-} 
-
-void createElement(char& elemenBaru){ 
-    cin>>elemenBaru; 
-} 
-
-void push(Stack& S, char elemenBaru) { 
-    if (S.TOP == maxElemen-1){ 
-        cout<<"Tumpukan sudah penuh"<<endl; 
-    } 
-    else{ 
-        S.TOP=S.TOP + 1; 
-        S.isi[S.TOP] = elemenBaru; 
-    } 
+void createList(Stack& TOP){
+	TOP = NULL;
 }
 
-void pop(Stack& S, char& elemenHsl){ 
-    if (S.TOP < 0) { 
-        cout<<"Tumpukan sudah kosong "<<endl; 
-    } 
-    else { 
-        elemenHsl= S.isi[S.TOP]; 
-        S.TOP=S.TOP - 1; 
-    } 
+void createElement (pointer& pBaru){
+	pBaru = new List;
+	cout << "Isi char : "; cin >> pBaru->isi;
+ 	pBaru->next = NULL;
 }
 
-void traversal(Stack S){ 
-    for (int i=S.TOP;i>=0;i--){ 
-        cout<<S.isi[i]<<" ";
-    } 
-} 
+void push (Stack& TOP, pointer pBaru){
+	if (TOP==NULL){
+		TOP=pBaru;
+	}
+	else{
+		pBaru->next=TOP;
+		TOP=pBaru;
+	}	
+}
 
+void pop (Stack& TOP, pointer& pHapus){
+	if (TOP==NULL){
+		cout << "Stack Kosong" <<endl;
+		pHapus=NULL;
+	}
+	else if (TOP->next==NULL){
+		pHapus=TOP;
+		TOP=NULL;
+ 	}
+ 	else{
+		pHapus=TOP;
+		TOP=TOP->next;
+  		pHapus->next=NULL;
+	}
+}
 
-int main(){ 
-    char elemenBaru; 
-    char elemenHasl;
-    Stack S; 
-    int n; 
-    createStack (S); 
-    cout<<"Banyak element: ";cin>>n; 
- cout<<"Input : ";
-    for (int i=0;i<n;i++){ 
-     createElement(elemenBaru); 
-        push(S,elemenBaru); 
-    } 
-    cout<<"Output pop : ";
- for (int i=0;i<n;i++){
-  pop(S, elemenHasl);
-  cout<<elemenHasl<<" ";
- }
- cout<<endl;
- 
- cout<<"Output : ";traversal(S);
-    
+void traversal (Stack TOP){
+	pointer pBantu;
+	pBantu=TOP;
+	if (TOP==NULL)
+  		cout << "List Kosong\n";
+	else
+ 		pBantu=TOP;
+	while(pBantu != NULL){
+		cout << pBantu->isi;
+		pBantu = pBantu->next;
+		cout<<endl;
+	}
+}
+
+int main(){
+	pointer p;
+	createList(S);
+	createElement(p);
+	push(S,p);
+	createElement(p);
+	push(S,p);
+	createElement(p);
+	push(S,p);
+
+	traversal(S);
 }
